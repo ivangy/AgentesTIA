ad00b4cc95f32847c6b02f41c6d02f6f0d1a7061
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2021-02-22 12:41:17 +0100

    Opciones menú creadas

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,10 +1,44 @@
 package Principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int opc;
+		
+		Scanner leer = new Scanner (System.in);
+		
+		
+		do {
+		
+			System.out.println("------------------------------------------------------");
+			System.out.println("Menú AgentesTIA");
+			System.out.println("------------------------------------------------------");
+			System.out.println("1 - Ver todos los agentes con su información");
+			System.out.println("2 - Ver los agentes que ganen más de una cantidad X");
+			System.out.println("3 - Dar de alta un nuevo piso");
+			System.out.println("4 - Dar de alta una nueva arma");
+			System.out.println("5 - Dar de alta un nuevo agente");
+			System.out.println("6 - Encriptar toda la información");
+			System.out.println("7 - Desencriptar toda la información");
+			System.out.println("8 - Salir");
+			System.out.println("");
+			
+			System.out.println("Dime una opción:");
+		
+			opc = leer.nextInt();
+			
+			if ((opc < 1) || (opc > 8)) {
+				System.out.println("Esta opcion es invalida, vuelve a intertarlo");
+			}
+		} while ((opc < 1) || (opc > 8));
+
+		
+		//return opc;   cambiar static void por static int 
 
 	}
-
+	
 }
+
