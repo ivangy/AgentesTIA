9e55fef7bb75cc4a6f7b5fc5c791c989c8282315
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2021-02-24 14:16:41 +0100

    estructura

--- src/AgentesTIA/AgenteEspia.java
+++ src/AgentesTIA/AgenteEspia.java
@@ -3,5 +3,27 @@ package AgentesTIA;
 public class AgenteEspia extends Agentes{
 	
 	private String pisos;
+	
+	public AgenteEspia (String nombre, int edad, String direccion, int salario, String pisos) {
+		
+		super(nombre, edad, direccion, salario);
+		
+		this.pisos = pisos;
+	}
+
+	public String getPisos() {
+		return pisos;
+	}
+
+	public void setPisos(String pisos) {
+		this.pisos = pisos;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "AgenteEspia [pisos=" + pisos + "]";
+	}
+	
+	
 
 }
