fd926744c52619f9dd56164da1e6f4222afceb0f
ivan <ivansanchez989898@gmail.com>
2021-03-07 01:29:55 +0100

    acabado Vectores dinamicos

--- src/AgentesTIA/Agente007.java
+++ src/AgentesTIA/Agente007.java
@@ -1,5 +1,6 @@
 package AgentesTIA;
 
+import java.security.Principal;
 import java.util.ArrayList;
 
 import IODatos.IODatos;
@@ -14,21 +15,43 @@ public final class Agente007 extends Agentes{
 	
 
 	private int muertes;
-	private String armas;
-	
-	public Agente007(String nombre, int edad, String direccion, float salario, int muertes, String armas) {
+	private ArrayList<String>armas=new ArrayList<String>();
+
+	public Agente007(String nombre, int edad, String direccion, float salario, int muertes) {
 		super(nombre, edad, direccion, salario);
 		this.muertes = muertes;
-		this.armas = armas;
+		this.armas = IODatos.leerDatos("Armas.txt", IODatos.so());
 	}
+
+
+
 
 	public int getMuertes() {
 		return muertes;
 	}
 
+
+
+
 	public void setMuertes(int muertes) {
 		this.muertes = muertes;
 	}
+
+
+
+
+	public ArrayList<String> getArmas() {
+		return armas;
+	}
+
+
+
+
+	public void setArmas(ArrayList<String> armas) {
+		this.armas = armas;
+	}
+
+
 
 
 	@Override
